f4e374f9b80799a9aa1954975dd51137b118f2d5
119mould <41019541+119mould@users.noreply.github.com>
2019-09-05 23:32:56 +0800

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>HTML嵌入式的脚本语言</title>
+</head>
+
+<body>
+<br/>
+
+<?php
+//第一个PHP文件
+//br表示换行
+echo "hello,php";
+?>
+</body>
+</html>
